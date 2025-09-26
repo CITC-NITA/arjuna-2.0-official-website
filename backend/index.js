e27d88d616e6cc66c02dc44e7a99f59ab1c36e2d
Abhishek Sakhare <abhisheksakhare2210@gmail.com>
2025-09-26 12:13:01 +0530

    fix: backend connected with vercel frontend url

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,14 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: ["https://arjuna-2-0-official-website.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
--- frontend/my-app/src/components/Hero.jsx
+++ frontend/my-app/src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link,useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import heroBg from '../assets/hero-bg.jpg';
 import "../styles/hero.css";
 import brochure from "../assets/brochure-pdf.pdf";
